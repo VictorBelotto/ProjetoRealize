72bd8bbdf7a71ca63c91737d7b276b1bd7c1dc59
VictorBelotto <108822052+VictorBelotto@users.noreply.github.com>
2022-07-08 14:04:11 -0300

    Delete SITE REALIZE directory

--- SITE REALIZE/js/index.js
+++
@@ -1,6 +0,0 @@
-const botaoMenu = document.querySelector('.icone__menu')
-const menu = document.querySelector('.menu-lateral')
-
-botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu-lateral--ativo')
-})
