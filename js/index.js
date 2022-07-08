f6278583ac15440140119edff2f8b41ac54d6f5f
VictorBelotto <victortucunduva.vb@outlook.com>
2022-07-08 16:39:12 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,3 +4,6 @@ const menu = document.querySelector('.menu-lateral')
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
 })
+
+
+var imagens = ["../img/"];
